74b958030c39ec60376d4500a311fab583178210
jun yamamoto <yamamoto@cocolomachi.co.jp>
2016-12-24 00:15:17 +0000

    gemgile upadate for heroku

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite!(@micropost)
+  end
+
+  def destroy
+    @micropost = Favorite.find(params[:id]).micropost
+    current_user.unfavorite!(@micropost)
+  end
+end
+
+
